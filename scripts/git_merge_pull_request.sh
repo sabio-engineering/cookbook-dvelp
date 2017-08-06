1061094d19e0d9cdce51d89f4fee05d40f8aa653
Tom Mullen <tom@dvelp.co.uk>
2017-08-06 21:04:26 +0100

    Rename branch_name to feature_branch
    Why:
    
    * The branch_name was a little ambiguous as it could be referring to
    either feature or target branch
    
    This change addresses the need by:
    
    * Renaming branch_name to feature_branch

--- scripts/git_merge_pull_request.sh
+++ scripts/git_merge_pull_request.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -e
-verbose='false'
 
-branch_name=`git symbolic-ref HEAD`
-branch_name=${branch_name##refs/heads/}
+feature_branch=`git symbolic-ref HEAD`
+feature_branch=${feature_branch##refs/heads/}
 target_branch='master'
+verbose='false'
 
 while getopts 'b:' flag; do
   case "${flag}" in
@@ -13,7 +13,7 @@ while getopts 'b:' flag; do
   esac
 done
 
-if [ "$branch_name" == $target_branch ]; then
+if [ "$feature_branch" == $target_branch ]; then
   echo "You're on $target_branch branch, checkout feature branch you want to merge"
   exit 1
 fi
@@ -30,16 +30,16 @@ git push -f
 echo "git checkout ${target_branch}"
 git checkout $target_branch
 
-echo "git merge $branch_name"
-git merge $branch_name
+echo "git merge $feature_branch"
+git merge $feature_branch
 
 echo "git push"
 git push
 
-echo "git push origin --delete $branch_name"
-git push origin --delete $branch_name
+echo "git push origin --delete $feature_branch"
+git push origin --delete $feature_branch
 
-echo "git branch -d $branch_name"
-git branch -d $branch_name
+echo "git branch -d $feature_branch"
+git branch -d $feature_branch
 
-echo "$branch_name has been merged into ${target_branch}. All is well!"
+echo "$feature_branch has been merged into ${target_branch}. All is well!"
